c9175302bb13d26ce05efba4eb56138dfb847c8a
VaroESP <varoclaver@gmail.com>
2025-11-11 19:07:02 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -13,27 +13,27 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 done
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Remove previous versions"
+echo -e "[${GREEN}OK${NC}] Remove previous versions"
 
 echo "[] Update package list and dependencies"
 sudo apt update > /dev/null 2>&1
 sudo install -y ca-certificates curl > /dev/null 2>&1
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Update package list and dependencies"
+echo -e "[${GREEN}OK${NC}] Update package list and dependencies"
 
 echo "[] Configure GPG directory"
 sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Configure GPG directory"
+echo -e "[${GREEN}OK${NC}] Configure GPG directory"
 
 echo "[] Download official Docker GPG key"
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>&1
 sudo chmod a+r /etc/apt/keyrings/docker.asc > /dev/null 2>&1
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Download official Docker GPG key"
+echo -e "[${GREEN}OK${NC}] Download official Docker GPG key"
 
 echo "[] Agregando repositorio oficial de Docker"
 echo \
@@ -42,24 +42,25 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Agregando repositorio oficial de Docker"
+echo -e "[${GREEN}OK${NC}] Agregando repositorio oficial de Docker"
 
 echo "[] Updating... again..."
 sudo apt update > /dev/null 2>&1
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Updating... again..."
+echo -e "[${GREEN}OK${NC}] Updating... again..."
 
 echo "[] Installing Docker components"
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
 echo -ne "\033[A"
 echo -ne "\033[K"
-echo "[${GREEN}OK${NC}] Installing Docker components"
+echo -e "[${GREEN}OK${NC}] Installing Docker components"
 echo
 
 echo "Checking Docker service status..."
 sudo systemctl status docker --no-pager
-
+echo
+echo
 echo "Commands for manual startup:"
 echo "	- sudo systemctl start docker"
 echo "	- sudo systemcrl enable docker"
