ea1ed01f28fb967388d3bda44eefb2e6c0f631c1
VaroESP <varoclaver@gmail.com>
2025-11-11 18:55:26 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,53 +2,63 @@
 # Author: varoESP
 # Description: Script to install Docker in Linux Ubuntu
 
+GREEN='\033[38;5;46m'
+NC='\033[0m'
+
 echo "#=== Starting the Docker installation ===#"
 
-echo "Removing previous versions"
+echo "[] Remove previous versions..."
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-	sudo apt remove -y $pkg > 2/dev/null 2>&1
+	sudo apt remove -y $pkg > /dev/null 2>&1
 done
-echo "[OK] Removing previous versions"
-echo
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Remove previous versions"
 
-echo "Updating package list and dependencies"
-sudo apt update > 2/dev/null 2>&1
-sudo install -y ca-certificates curl > 2/dev/null 2>&1
-echo "[OK] Updating package list and dependencies"
-echo
+echo "[] Update package list and dependencies"
+sudo apt update > /dev/null 2>&1
+sudo install -y ca-certificates curl > /dev/null 2>&1
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Update package list and dependencies"
 
-echo "Installing dependencies"
-sudo install -m 0755 -d /etc/apt/keyrings > 2/dev/null 2>&1
-echo "[OK] Installing dependencies"
-echo
+echo "[] Configure GPG directory"
+sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Configure GPG directory"
 
-echo "Downloading official Docker GPG key"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > 2/dev/null 2>&1
-sudo chmod a+r /etc/apt/keyrings/docker.asc > 2/dev/null 2>&1
-echo "[OK] Downloading official Docker GPG key"
-echo
+echo "[] Download official Docker GPG key"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>&1
+sudo chmod a+r /etc/apt/keyrings/docker.asc > /dev/null 2>&1
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Download official Docker GPG key"
 
 echo "Agregando repositorio oficial de Docker"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo "[OK] Agregando repositorio oficial de Docker"
-echo
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Agregando repositorio oficial de Docker"
 
 echo "Updating... again..."
-sudo apt update > 2/dev/null 2>&1
-echo "[OK] Updating... again..."
-echo
+sudo apt update > /dev/null 2>&1
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Updating... again..."
 
 echo "Installing Docker components"
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > 2/dev/null 2>&1
-echo "[OK] Installing Docker components"
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
+echo -ne "\033[A"
+echo -ne "\033[K"
+echo "[${GREEN}OK${NC}] Installing Docker components"
 echo
 
 echo "Checking Docker service status..."
 sudo systemctl status docker --no-pager
-echo
 
 echo "Commands for manual startup:"
 echo "	- sudo systemctl start docker"
